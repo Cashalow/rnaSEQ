f2aeef85a521549c193c2a8ff05c0f3361248c55
Cashalow <sachalau@gmail.com>
2017-03-20 10:23:00 +0100

    Creating file for extracting overrepresented reads

---
+++ extractOverrrepresentedSequences.sh
@@ -0,0 +1,7 @@
+for i in $(ls *_fastqc)
+do
+  cd $i;
+  sed -n '/Overrepresented/,$p' fastqc_data.txt | sed '/>>END_MODULE/q' | tail -n +2 | head -n -1 > overrepresented_sequences.txt
+  tail -n +2 overrepresented_sequences.txt | cut -f 1 | sed -e 's/^/>Seq\n/' > overrepresented_sequences.fasta
+  tail -n +2 overrepresented_sequences.txt | cut -f 3 | awk '{s+=$1} END {print s}' > total_percent_overrepresented.txt
+  cd ..;
